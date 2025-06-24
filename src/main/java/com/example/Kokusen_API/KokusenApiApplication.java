07d14e739ffb32b9aa8e5f121a9651ca58eeeb62
mauricioandrade <mauricio_andrade@outlook.com>
2025-06-23 23:31:07 -0300

    teste

---
+++ src/main/java/com/example/Kokusen_API/KokusenApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kokusen_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokusenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KokusenApiApplication.class, args);
+	}
+
+}
